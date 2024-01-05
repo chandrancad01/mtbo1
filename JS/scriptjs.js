7ff03e41c4cefdfb441cb38efa562df05dfeaf5b
chandrancad01 <chandrancad01@gmail.com>
2024-01-05 18:08:19 +0530

    added to js/script.js and demo.html and img.1jpg.png

--- JS/scriptjs.js
+++ JS/scriptjs.js
@@ -2,3 +2,62 @@ console.log("welcome to javascript");
 console.log(10.10);
 console.log({ fname:"chandran", gender:"male"});
 console.table({ fname:"chandran", gender:"male"});
+
+a=(10);
+console.log(typeof a);
+console.log(a);
+
+b=(15);
+console.log(a+b);
+console.log([1,23,45,14]);
+console.log(true);
+console.log(b);
+
+console.log(typeof b);
+
+
+
+console.log(x*y);
+console.log(x+y);
+console.log(z-x);
+console.log(y%z);
+
+
+console.log(x+y+z);
+
+
+console.log(x==5);
+console.log(x==20);
+console.log(y==10 || y>5);
+
+console.log(x+=y);
+console.log(z-=x);
+console.log(y*=x);
+console.log(x%=y);
+
+
+//relational operator
+
+var x=20;
+var y=15;
+var z=10;
+
+
+
+
+console.log(x>y);
+console.log(x>z);
+console.log(x>=5);
+console.log(x>=y);
+console.log(z<=25);
+console.log(x<=z);
+
+//logical operator
+
+var x=20;
+var y=15;
+var z=10;
+
+console.log(x==y || z<x || x>z);
+console.log(x<=z || x==20 || x>y);
+console.log(!(x<="z"));
