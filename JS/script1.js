a57d211436b235d71927dca8867fda5f8a701c64
chandrancad01 <chandrancad01@gmail.com>
2024-01-12 18:10:27 +0530

    added files

---
+++ JS/script1.js
@@ -0,0 +1,19 @@
+function RegisterForm()
+{
+    var USERNAME1=document.getElementById("USERNAME").value;
+        document.getElementById("UNAME").innerHTML=USERNAME1;
+
+        var USERNAME2=document.getElementById("firstname").value;
+        document.getElementById("fname").innerHTML=USERNAME2;
+
+        var USERNAME3=document.getElementById("Lastneme").value;
+        document.getElementById("lname").innerHTML=USERNAME3;
+
+        var USERNAME4=document.getElementById("dob").value;
+        document.getElementById("date").innerHTML=USERNAME4;
+
+        var USERNAME5=document.getElementById("male").value;
+        document.getElementById("gen").innerHTML=USERNAME5;
+
+}
+    console.log("UNAME");
